c013f35b85be79da6f4ff7cc114e323618aeebc8
joe <joe@joetify.com>
2011-10-02 04:44:00 -0700

    folsom http interface

--- apps/antplus_websockets/src/antplus_websockets_sup.erl
+++ apps/antplus_websockets/src/antplus_websockets_sup.erl
@@ -20,5 +20,7 @@ init(Options) ->
                    permanent, 60000, worker, [antplus_websockets_server]
                   },
 
-    {ok, {{one_for_all, 5, 30}, [MisultinSpec, ServerSpec]}}.
+    Folsom = {folsom_webmachine, {folsom_webmachine_sup, start_link, []}, permanent, infinity, supervisor, [folsom_webmachine_sup]}, 
+
+    {ok, {{one_for_all, 5, 30}, [MisultinSpec, ServerSpec, Folsom]}}.
 
